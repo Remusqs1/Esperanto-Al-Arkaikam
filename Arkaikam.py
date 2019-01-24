f823e96efe1fc4fa352ef562bd9713ba3ffb45e5
Remusqs1 <ivanca2291@hotmail.com>
2019-01-24 15:03:57 -0500

    Pluraj ŝanĝoj: Renomado de dosiero, la dosiero nun permesas al uzanto enigi frazon, malmajuskligas ĝin kaj dividas la frazon je vortoj. Aldono de .gitignore

---
+++ Arkaikam.py
@@ -0,0 +1,13 @@
+Frazo=input("Enigu vian frazon: ")
+frazo=Frazo.lower()
+print(frazo)
+
+
+#Dividas la vortojn de la frazo laŭ malplena spaco. Poste ĉiu ero estos legita kaj kategoriita laŭ finaĵo.
+ĉeno=frazo.split(" ")
+print(ĉeno)
+
+# print(ĉeno[0])
+# print(ĉeno[1])
+# print(ĉeno[2])
+# print(len(ĉeno))
