31405d5e26fef5e07c21e6ae3c0de9a3274cbbe3
Remusqs1 <ivanca2291@hotmail.com>
2019-01-24 20:40:43 -0500

    Aldono de dosiero kun funkcioj uzataj en la ĉefdosiero Arkaikam, kroma dosiertipo en gitignore

--- Arkaikam.py
+++ Arkaikam.py
@@ -1,13 +1,25 @@
+### La celo de ĉi programeto estas ke uzanto enmetu frazon, ekz: Mi estas frazo,
+### kaj la programo redonu tutan tradukon al Arkaikam: Mihi estams Frazom
+
+from funkcioj import *
+
 Frazo=input("Enigu vian frazon: ")
 frazo=Frazo.lower()
-print(frazo)
-
 
 #Dividas la vortojn de la frazo laŭ malplena spaco. Poste ĉiu ero estos legita kaj kategoriita laŭ finaĵo.
 ĉeno=frazo.split(" ")
-print(ĉeno)
+print(ĉeno)#Forigota, nune nur por vidi la ĉenon
 
-# print(ĉeno[0])
-# print(ĉeno[1])
-# print(ĉeno[2])
-# print(len(ĉeno))
+for i in range(len(ĉeno)):
+
+    lastaLitero=ĉeno[i][-1] #Legas la lastan literon de vorto
+
+    #Jen funkcio por scii ĉu vorto estas substantivo aŭ ne, se jes, konjugacias arkaike
+    if lastaLitero=="o":
+        print(ĉeno[i] + ' estas substantivo') #ĉeno[i] estas vorto, ĉi tio forigotos
+        
+        #Konjugacio arkaika
+        vortoAE=substantivoSingulara(ĉeno[i])
+        print(ĉeno[i] + " tradukita al Arkaikam estus: " + vortoAE)
+    else:
+        print(ĉeno[i] + ' ne estas substantivo')
---
+++ funkcioj.py
@@ -0,0 +1,9 @@
+# vorto=input("Enigu frazon: ") #Tio ĉi forigeblas, nur estas por ke 'vorto' eniru en la funkcio
+
+def substantivoSingulara(vorto):
+    Vorto=vorto.capitalize()#Arkaike, substantivo estas ĉiam skribita majuskle je kia ajn kazo
+    Vorto=Vorto+"m"
+
+    return (Vorto)
+
+# substantivoSingulara(vorto)
