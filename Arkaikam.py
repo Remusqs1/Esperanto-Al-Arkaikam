19f5f159fada9f85ccbaec144d9509e33d3f1d10
Remusqs1 <Remusqs1@gmail.com>
2019-07-16 16:36:19 -0500

    Listo de specialaj vortoj kun specialaj tradukoj

--- Arkaikam.py
+++ Arkaikam.py
@@ -9,7 +9,6 @@ frazo=input("Enigu vian frazon: ").lower()
 ĉeno=frazo.split(" ")
 
 #Forigas la difinitan artikolon "la", TODO kie enmeti cxi tion unue?
-
 ĉeno=ArtikoloDifinitaForigado(ĉeno) #ĉeno estas anstataŭata per la renodaĵo el ArtikoloDifinitaForigado
 SpecialajŜanĝoj(ĉeno)
 
