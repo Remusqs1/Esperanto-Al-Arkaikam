b0cdfe1d659b33526fb9d10fafff876c39364fad
Ivan Camilo Quintero Santacruz <ivanca2291@hotmail.com>
2023-07-13 14:18:13 -0500

    Sendependigo de grafika interfaco en aparta dosiero

--- Arkaikam.py
+++ Arkaikam.py
@@ -1,12 +1,12 @@
 ### La celo de ĉi programeto estas ke uzanto enmetu frazon, ekz: Mi estas frazo,
 ### kaj la programo redonu tutan tradukon al Arkaikam: Mihi estams Frazom
 import tkinter as tk
-
+from vidInterfaco import krei_gui
 from funkcioj import *
 from vortaroj import specialajVortoj
 from iloj import *
 
-def arkaikam():
+def arkaikam(kadro, rezultaKadro):
 
     #TODO nuntempe ne uzu interpunkcion
     frazo = kadro.get("1.0",'end-1c').strip().lower()
@@ -34,41 +34,5 @@ def arkaikam():
     rezultaKadro.insert(tk.END, frazo)
     rezultaKadro.configure(state="disabled")
 
-#region agordoj de aplikaĵa fenestro
-
-#Ĉefa fenestro 
-fenestro = tk.Tk()
-fenestro.title("Tujtradukilo el Esperanto al Arkaika Esperanto")
-fenestro.geometry("750x200")
-fenestro.resizable(False,False)
-
-# Mesaĝa kadro
-kadro = tk.Text(fenestro, width = 84, height = 3)
-kadro.place(x=10, y=25)
-
-teksto = tk.Label(fenestro, text = "Enigu vian frazon:")
-teksto.place(x=10, y=2)
-
-#Butono
-butono = tk.Button(fenestro, text = "Traduki", command = arkaikam)
-butono.place(x=330, y=85)
-
-#Rezulta kadro
-rezultaKadro = tk.Text(fenestro, state="disabled", width = 84, height = 3)
-rezultaKadro.place(x=10, y=135)
-
-traduko = tk.Label(fenestro, text = "Traduko:")
-traduko.place(x=10, y=110)
-#endregion
-
-#Kopii butonoj
-
-#Origina teksto
-butonoKopii = tk.Button(fenestro, text = "Kopii", command =  lambda: kopii(kadro))
-butonoKopii.place(x=700, y=25)
-
-#tradukita teksto
-butonoTraduko = tk.Button(fenestro, text = "Kopii", command =  lambda: kopii(rezultaKadro))
-butonoTraduko.place(x=700, y=135)
-
-fenestro.mainloop()
+if __name__ == "__main__":
+    krei_gui(lambda kadro, rezultaKadro: arkaikam(kadro, rezultaKadro))
---
+++ vidInterfaco.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+
+def krei_gui(arkaikam_revoko):
+    #Ĉefa fenestro 
+    fenestro = tk.Tk()
+    fenestro.title("Tujtradukilo el Esperanto al Arkaika Esperanto")
+    fenestro.geometry("750x200")
+    fenestro.resizable(False,False)
+
+    # Mesaĝa kadro
+    kadro = tk.Text(fenestro, width = 84, height = 3)
+    kadro.place(x=10, y=25)
+
+    teksto = tk.Label(fenestro, text = "Enigu vian frazon:")
+    teksto.place(x=10, y=2)
+
+    def traduki():
+        arkaikam_revoko(kadro, rezultaKadro)
+
+    #Butono
+    butono = tk.Button(fenestro, text = "Traduki", command = traduki)
+    butono.place(x=330, y=85)
+
+    #Rezulta kadro
+    rezultaKadro = tk.Text(fenestro, state="disabled", width = 84, height = 3)
+    rezultaKadro.place(x=10, y=135)
+
+    traduko = tk.Label(fenestro, text = "Traduko:")
+    traduko.place(x=10, y=110)
+
+    #Kopii butonoj
+
+    #Origina teksto
+    butonoKopii = tk.Button(fenestro, text = "Kopii", command =  lambda: kopii(kadro))
+    butonoKopii.place(x=700, y=25)
+
+    #tradukita teksto
+    butonoTraduko = tk.Button(fenestro, text = "Kopii", command =  lambda: kopii(rezultaKadro))
+    butonoTraduko.place(x=700, y=135)
+
+    fenestro.mainloop()
